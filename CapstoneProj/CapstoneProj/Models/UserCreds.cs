e2aacfbd2ae8f5905313b983b0e9cf8e58bc44c3
GothicLolita229 <ceciliathomas4216@gmail.com>
2023-02-18 19:11:04 -0500

    Created a Model named Usercreds
    Created a class under the Models folder named usercreds and it's to base the controller off of it. But I think we need a database to connect to before moving forward with it

---
+++ CapstoneProj/CapstoneProj/Models/UserCreds.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneProj.Models
+{
+	public class UserCreds
+	{
+		public int ID { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+
+		public string password { get; set; }
+	}
+}
