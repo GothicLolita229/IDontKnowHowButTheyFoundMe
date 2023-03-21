c81d898dc20b2377d595f4c8ae9f3f2c7930d304
GothicLolita229 <ceciliathomas4216@gmail.com>
2023-03-21 18:40:00 -0400

    Added Class Library
    Added Class Library and Kalie's Encryption code to it with the EncryptPassword(string) method

---
+++ CapstoneProject/TastyTableLibrary/Encryption.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace TastyTableLibrary
+{
+    public class Encryption
+    {
+		public static string EncryptPassword(string password)
+		{
+			using (SHA256 sha256 = SHA256.Create())
+			{
+				byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+				StringBuilder builder = new StringBuilder();
+				for (int i = 0; i < hashBytes.Length; i++)
+				{
+					builder.Append(hashBytes[i].ToString("x2"));
+				}
+				return builder.ToString();
+			}
+		}
+	}
+}
