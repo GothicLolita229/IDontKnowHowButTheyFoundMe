252f4c44efadb97b8ca24f4b915906f4ea70bac7
CiaraM100104 <90720471+CiaraM100104@users.noreply.github.com>
2023-03-22 20:25:27 -0400

    Basic input to txt file generator
    Has a savefile dialog, had to add in windows forms in order to work, will be edited with progress.

---
+++ CapstoneProject/TastyTableLibrary/InputToTxT.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TastyTableLibrary
+{
+    public class InputToTxT
+    {
+        public static void InputToTextFile(string userInput)
+        {
+            try
+            {
+                StreamWriter outputFile;
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            }
+        }
+    }
+}
